5816fad0a022caba4272e4317d7472786960571f
Brayan <Brayan@brayan.tigo.co.cr>
2015-09-17 21:53:56 -0600

    modelo

--- Paradigmas1/src/paradigmas1/Paradigmas1.java
+++ Paradigmas1/src/paradigmas1/Paradigmas1.java
@@ -15,7 +15,7 @@ public class Paradigmas1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola Mundo por Esteban de Belen");
+        System.out.println("Hola Mundo por Bryan de Heredia");
     }
     
 }
