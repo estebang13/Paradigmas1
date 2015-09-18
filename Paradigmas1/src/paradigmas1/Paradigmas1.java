1a73965a7a4a8e7935df87b2e5ad884a74cc3fc3
Brayan <Brayan@brayan.tigo.co.cr>
2015-09-17 21:33:11 -0600

    prueba 1

--- Paradigmas1/src/paradigmas1/Paradigmas1.java
+++ Paradigmas1/src/paradigmas1/Paradigmas1.java
@@ -15,7 +15,7 @@ public class Paradigmas1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+        System.out.println("Hola Mundo por Esteban de Belen");
     }
     
 }
