eeb6863bc4087d35dbf559eeadfd3c2bbc71ce7b
mac <mac@192.168.1.8>
2015-10-04 22:20:49 -0600

    Algorithms2

--- Paradigmas1/src/Modelo/Algorithms.java
+++ Paradigmas1/src/Modelo/Algorithms.java
@@ -39,33 +39,64 @@ public class Algorithms {
         }
         return false;
     }
-    
-     public static String BinarySuma(String a, String b) {
+
+    public static String BynaryUnary(String a) {
+        int a1 = Integer.parseInt(a, 2);
+        String sResult1 = "";
+        for (int x = 0; x < a1; x++) {
+            sResult1 = sResult1 + "|";
+        }
+        return sResult1;
+    }
+
+    public static String BinarySuma(String a, String b) {
         int a1 = Integer.parseInt(a, 2);
         int b1 = Integer.parseInt(b, 2);
         int c1 = a1 + b1;
         String result = Integer.toString(c1, 2);
         return result;
     }
-     
-         public static double DecimalSuma(double n,double m){
-             double c=n+m;
-             return c; 
+    
+    public static String TriplicateBinary(String a) {
+        int a1 = Integer.parseInt(a, 2);
+        int c1 = a1 * 3;
+        String result = Integer.toString(c1, 2);
+        return result;
     }
-  
+
+    public static double DecimalSuma(double n, double m) {
+        double c = n + m;
+        return c;
+    }
+
+    public static String DoubleChar(String a) {
+        String sResult1 = "";
+        char b = ' ';
+        sResult1 = sResult1+a.charAt(0); 
+        for (int x = 1; x < a.length(); x++) {   
+        b = a.charAt(x-1);
+        char c =  a.charAt(x);    
+         if (b!=c)
+            sResult1 = sResult1 + c;
+        }
+        return sResult1;
+    }
 
     public static void main(String args[]) {
         String sCadena = "LineaDeCodigo";
-        String B1="1111";
-        String B2="1110";
-        String B3="1101";
+        String sCadena1 = "aaaaabbbbbbcccckkkkkkkkkkkjsjsssss";
+        String B1 = "1111";
+        String B2 = "1110";
+        String B3 = "1101";
         Algorithms a = new Algorithms();
         System.out.println(a.TurnLine(sCadena));
         System.out.println(a.DuplicateLine(sCadena));
-        System.out.println("Ana es Palindromo:"+a.Palindromo("Ana"));
-        System.out.println("Luis es Palindromo:"+a.Palindromo("Luis"));
-        System.out.println("Suma"+B1+""+"+"+B2+"="+a.BinarySuma(B1,B2));
-        System.out.println("Suma"+B1+""+"+"+B3+"="+a.BinarySuma(B1,B3));
-        System.out.println("Suma: "+"2.0"+""+"+"+"5.0"+"="+a.DecimalSuma(2.0,5.0));
+        System.out.println(a.BynaryUnary(B1));
+        System.out.println("Ana es Palindromo:" + a.Palindromo("Ana"));
+        System.out.println("Luis es Palindromo:" + a.Palindromo("Luis"));
+        System.out.println("Suma" + B1 + "" + "+" + B2 + "=" + a.BinarySuma(B1, B2));
+        System.out.println("Triple: " + a.TriplicateBinary(B3));
+        System.out.println("Suma: " + "2.0" + "" + "+" + "5.0" + "=" + a.DecimalSuma(2.0, 5.0));
+        System.out.println(a.DoubleChar(sCadena1));
     }
 }
